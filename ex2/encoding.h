06648da9ef4bfd3b9366598b9cfeabeb6e07951e
andrewstephengames <andrewstephengames999@gmail.com>
2024-05-27 08:34:24 +0300

    Finish problem 2

--- ex2/encoding.h
+++ ex2/encoding.h
@@ -1,7 +1,143 @@
 #ifndef ENCODING_H
 #define ENCODING_H
 
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <vector>
 #include <iostream>
+
 using namespace std;
+
+class TreeNode {
+public:
+    char key;
+    int freq;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(char k, int f) : key(k), freq(f), left(nullptr), right(nullptr) {}
+};
+
+class BST {
+public:
+    TreeNode* root;
+
+    BST() : root(nullptr) {}
+
+    void insert(char key, int freq) {
+        root = insertRec(root, key, freq);
+    }
+
+    TreeNode* removeMin() {
+        if (!root) return nullptr;
+        TreeNode* minNode = nullptr;
+        root = removeMinRec(root, minNode);
+        return minNode;
+    }
+
+private:
+    TreeNode* insertRec(TreeNode* node, char key, int freq) {
+        if (!node) return new TreeNode(key, freq);
+        if (freq < node->freq)
+            node->left = insertRec(node->left, key, freq);
+        else
+            node->right = insertRec(node->right, key, freq);
+        return node;
+    }
+
+    TreeNode* removeMinRec(TreeNode* node, TreeNode*& minNode) {
+        if (!node->left) {
+            minNode = node;
+            return node->right;
+        }
+        node->left = removeMinRec(node->left, minNode);
+        return node;
+    }
+};
+
+class EncodingTree {
+public:
+    TreeNode* root;
+
+    EncodingTree(BST& bst) {
+        auto comp = [](TreeNode* a, TreeNode* b) { return a->freq > b->freq; };
+        priority_queue<TreeNode*, vector<TreeNode*>, decltype(comp)> pq(comp);
+
+        while (TreeNode* minNode = bst.removeMin()) {
+            pq.push(minNode);
+        }
+
+        if (pq.empty()) {
+            root = nullptr;
+            return;
+        }
+
+        while (pq.size() > 1) {
+            TreeNode* left = pq.top(); pq.pop();
+            TreeNode* right = pq.top(); pq.pop();
+            TreeNode* newNode = new TreeNode('\0', left->freq + right->freq);
+            newNode->left = left;
+            newNode->right = right;
+            pq.push(newNode);
+        }
+
+        root = pq.top();
+    }
+};
+
+void buildEncodingMap(TreeNode* node, const string& str, unordered_map<char, string>& encodingMap) {
+    if (!node) return;
+    if (node->key != '\0') {
+        encodingMap[node->key] = str;
+    }
+    buildEncodingMap(node->left, str + "0", encodingMap);
+    buildEncodingMap(node->right, str + "1", encodingMap);
+}
+
+string encode(TreeNode* root, const string& text) {
+    unordered_map<char, string> encodingMap;
+    buildEncodingMap(root, "", encodingMap);
+
+    string encoded;
+    for (char ch : text) {
+        encoded += encodingMap[ch];
+    }
+
+    return encoded;
+}
+
+string decode(TreeNode* root, const string& encoded) {
+    string decoded;
+    TreeNode* currentNode = root;
+    for (char bit : encoded) {
+        if (currentNode == nullptr) {
+            cerr << "Error: currentNode is null." << endl;
+            return "";
+        }
+        currentNode = (bit == '0') ? currentNode->left : currentNode->right;
+        if (currentNode && !currentNode->left && !currentNode->right) {
+            decoded += currentNode->key;
+            currentNode = root;
+        }
+    }
+    return decoded;
+}
+
+BST buildFrequencyBST(const string& text) {
+    unordered_map<char, int> freqMap;
+    for (char ch : text) {
+        if (isalnum(ch) || ch == ' ') {
+            freqMap[ch]++;
+        }
+    }
+
+    BST bst;
+    for (const auto& pair : freqMap) {
+        bst.insert(pair.first, pair.second);
+    }
+
+    return bst;
+}
 
 #endif // ENCODING_H
--- ex2/main.cpp
+++ ex2/main.cpp
@@ -1,6 +1,55 @@
+#include <iostream>
+#include <fstream>
 #include "encoding.h"
 
-int main () {
-    cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    ifstream inputFile("message.in");
+    if (!inputFile.is_open()) {
+        cerr << "Error opening message.in file." << endl;
+        return 1;
+    }
+
+    string text((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
+    inputFile.close();
+
+    BST bst = buildFrequencyBST(text);
+
+    EncodingTree encodingTree(bst);
+
+    if (encodingTree.root == nullptr) {
+        cerr << "Error: Encoding tree root is null." << endl;
+        return 1;
+    }
+
+    string encodedMessage = encode(encodingTree.root, text);
+
+    ofstream encodedOutFile("encoded.out");
+    if (!encodedOutFile.is_open()) {
+        cerr << "Error opening encoded.out file." << endl;
+        return 1;
+    }
+    encodedOutFile << encodedMessage;
+    encodedOutFile.close();
+
+    ifstream encodedInFile("encoded.in");
+    if (!encodedInFile.is_open()) {
+        cerr << "Error opening encoded.in file." << endl;
+        return 1;
+    }
+    string encodedInput((istreambuf_iterator<char>(encodedInFile)), istreambuf_iterator<char>());
+    encodedInFile.close();
+
+    string decodedMessage = decode(encodingTree.root, encodedInput);
+
+    ofstream decodedOutFile("message.out");
+    if (!decodedOutFile.is_open()) {
+        cerr << "Error opening message.out file." << endl;
+        return 1;
+    }
+    decodedOutFile << decodedMessage;
+    decodedOutFile.close();
+
     return 0;
 }
